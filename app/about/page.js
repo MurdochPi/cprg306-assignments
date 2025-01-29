b0968ef33f4582b5b272aaa6e26a838400c1d09b
MurdochPi <murdoch.piercey@edu.sait.ca>
2025-01-28 21:56:27 -0700

    First push

---
+++ app/about/page.js
@@ -0,0 +1,26 @@
+export default function Pge() 
+{
+    return (
+        <>
+        <h1>About Page</h1>
+        <Movie>
+            
+        </Movie>
+        </>
+    );
+}
+
+function Movie() 
+{
+    return (
+        <>
+            <h2>
+                Catch me if you can
+            </h2>
+            <h3>
+                Leonardo Di Caprio
+            </h3>
+            <img src=""></img>
+        </>
+    )
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Murdoch Piercey</h1>
+        <Link href="https://github.com/MurdochPi/cprg306-assignments.git">https://github.com</Link>
+      </main>
+    );
+  }
